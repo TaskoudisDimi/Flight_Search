0a6e0707027fb860966ea2c9b2c150b88ad4c6f7
DimitrisTaskoudis <taskoudisdimitris@gmail.com>
2025-04-13 18:33:01 +0300

    change the requirements.txt

--- app.py
+++ app.py
@@ -16,3 +16,4 @@ app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
+
--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 
 class Config:
-    AMADEUS_CLIENT_ID = os.environ.get('AMADEUS_CLIENT_ID', 'your_client_id')
-    AMADEUS_CLIENT_SECRET = os.environ.get('AMADEUS_CLIENT_SECRET', 'your_client_secret')
+    AMADEUS_CLIENT_ID = os.environ.get('AMADEUS_CLIENT_ID', 'miTq9txNAlSY1q68fGwDP8E5bFQ8OPVo')
+    AMADEUS_CLIENT_SECRET = os.environ.get('AMADEUS_CLIENT_SECRET', 'QN6hvjLj2KVFfkUG')
